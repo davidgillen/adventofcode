fffd8a13a95608754e6d89c5893b5435737a2369
David Gillen <davidgillen@gmail.com>
2020-12-07 16:07:05 +0000

    Minor Day 5 update

--- 2020/Day5.2.py
+++ 2020/Day5.2.py
@@ -2,13 +2,11 @@ fp = open('Day5.1.input.txt')
 lines = fp.readlines()
 
 def getRowNumber(row):
-    row = row.replace('F', '0')
-    row = row.replace('B', '1')
+    row = row.replace('F', '0').replace('B', '1')
     return int(row, 2)
 
 def getSeatNumber(seat):
-    seat = seat.replace('L', '0')
-    seat = seat.replace('R', '1')
+    seat = seat.replace('L', '0').replace('R','1')
     return int(seat, 2)
 
 seatIds = []
