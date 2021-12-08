99ce84bcff6720b5092b34d350ebafaa8259e879
David Gillen <davidgillen@gmail.com>
2021-12-08 11:52:39 +0000

    Day 8 second part done.

--- 2021/Day8.2.py
+++ 2021/Day8.2.py
@@ -1,11 +1,13 @@
-fp = open('Input8.sample')
-#fp = open('Input8.txt')
+#fp = open('Input8.sample')
+fp = open('Input8.txt')
 
 class Output:
     output = {}
     outputDisp = [0,0,0,0,0,0,0,0,0,0]
 
     def __init__(self, wirings):
+        self.output = {}
+        self.outputDisp = [0,0,0,0,0,0,0,0,0,0]
         for wiring in wirings.split(' '):
             wiring = ''.join(sorted(wiring))
             if len(wiring) == 2:
@@ -37,6 +39,7 @@ class Output:
         # Find 6, 1 is not a sub set
         for key in self.output.keys():
             if len(key) == 6 and 0 in [c in key for c in self.outputDisp[1]]:
+                print('Found 6')
                 self.output[key] = [6]
                 self.outputDisp[6] = key
         for key in self.output.keys():
@@ -46,28 +49,46 @@ class Output:
         # Find 5, a subset of 6, and also now 2
         for key in self.output.keys():
             if len(key) == 5 and 0 not in [c in self.outputDisp[6] for c in key]:
+                print('Found 5')
                 self.output[key] = [5]
                 self.outputDisp[5] = key
         for key in self.output.keys():
             if len(key) == 5 and key != self.outputDisp[3] and key != self.outputDisp[5]:
+                print('Found 2')
                 self.output[key] = [2]
 
         # Find 9, 5 is a sub set, and also no 0
         for key in self.output.keys():
             if len(key) == 6 and key != self.outputDisp[6] and 0 not in [c in key for c in self.outputDisp[5]]:
+                print('Found 9')
                 self.output[key] = [9]
                 self.outputDisp[9] = key
         for key in self.output.keys():
             if len(key) == 6 and key != self.outputDisp[6] and key != self.outputDisp[9]:
+                print('Found 0')
                 self.output[key] = [0]
+
+    def calcValue(self, outputs):
+        digits = ''
+        for digit in outputs.split(' '):
+            digit = ''.join(sorted(digit))
+            digits += str(self.output[digit][0])
+        print(int(digits))
+        return int(digits)
+
+
             
-count = 0
+sum = 0
 for line in fp.readlines():
-    line = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
+    #line = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
     wirings, outputs = line.strip().split(' | ')
+    print(wirings)
     disp = Output(wirings)
-    print(disp.outputDisp)
     print(disp.output)
-    break
-    exit()
-print(count)
+    
+    #print(disp.outputDisp)
+    #print(disp.output)
+    sum += disp.calcValue(outputs)
+    print()
+
+print(sum)
