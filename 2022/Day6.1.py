0a44bfd1859ee8d4e2d928ee3dde113cf10fc505
David Gillen <davidgillen@gmail.com>
2022-12-06 10:47:33 +0000

    Day6 Solution

---
+++ 2022/Day6.1.py
@@ -0,0 +1,15 @@
+fp = open('input6.txt')
+line = fp.readline()
+i = 0
+marker = 4
+while i < (len(line)-marker):
+    snippet = line[i:i+marker]
+    j = 0
+    while j < marker:
+        if snippet.count(snippet[j]) > 1:
+            break
+        j += 1
+    if j == marker:
+        print('HERE: ' + str(i+marker))
+        exit()
+    i += 1
